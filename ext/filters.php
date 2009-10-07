28e479c835b740a5c6a8d1a9834603aea521ceb4
girsbrai <girsbrai@power.fsmedia.net>
2009-10-07 12:30:34 -0400

    Node tests pass

--- ext/filters.php
+++ ext/filters.php
@@ -2,6 +2,6 @@
 
 require_once H2O_PATH.'/h2o/filter.php';
 
-$strings = array('upper' => 'strtoupper', 'lower' => 'strtolower', 'titlecase' => 'ucwords', 'escape' => 'htmlspecialchars');
+$strings = array('upper' => 'strtoupper', 'lower' => 'strtolower', 'capitalize' => 'ucwords', 'escape' => 'htmlspecialchars');
 
 h2o_Filter::add($strings);
